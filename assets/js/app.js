261af931bc6c2b7f263923dc52977f388af7e956
Said Pelaez <diego.210@hotmail.es>
2017-11-11 06:43:35 -0600

    adding callback to loadjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
-var load = function (path, target) {
+var load = function (path, target, callback) {
     if (target === void 0) { target = "#content"; }
     $.get(path).done(function (data) {
         $(target).html(data);
+        if (callback !== null && callback !== undefined) {
+            callback();
+        }
     });
 };
 ///<reference path='main_functions.ts' /> 
--- dev/ts/main_functions.ts
+++ dev/ts/main_functions.ts
@@ -1,5 +1,8 @@
-var load = function(path, target = "#content"){
+var load = function(path, target = "#content", callback){
 	$.get(path).done(function (data) {
 		$(target).html(data);
+		if (callback !== null && callback !== undefined) {
+			callback();
+		}
 	})
 };
