340998b77a7bd558f0f566cf50da76ef6bd4f65d
Said Pelaez <diego.210@hotmail.es>
2017-11-11 10:13:00 -0600

    View 2 ready

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,23 @@ var load = function (path, target, callback) {
         if (callback !== null && callback !== undefined) {
             callback();
         }
+        else {
+            $("#header").css("display", "block");
+        }
     });
+};
+var landing = function () {
+    load("landing.html", "#content", function () {
+        $("#header").css("display", "none");
+    });
+};
+var load_business_form = function () {
+    var checked = document.getElementById("empresa").checked;
+    if (checked) {
+        load("business.html", "#business");
+    }
+    else {
+        $("#business").html("");
+    }
 };
 ///<reference path='main_functions.ts' /> 
--- dev/ts/main_functions.ts
+++ dev/ts/main_functions.ts
@@ -3,6 +3,24 @@ var load = function(path, target = "#content", callback){
 		$(target).html(data);
 		if (callback !== null && callback !== undefined) {
 			callback();
+		}else{
+			$("#header").css("display", "block");
 		}
 	})
 };
+
+var landing = function(){
+	load("landing.html","#content", function(){
+		$("#header").css("display", "none");
+	});
+}
+
+var load_business_form = function(){
+	var checked = document.getElementById("empresa").checked;
+
+	if(checked){
+		load("business.html", "#business")
+	}else{
+		$("#business").html("");
+	}
+}
